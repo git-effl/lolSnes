cd9323556110ba81b1748c14d9fc2a43405f1c9d
git-effl <serenatamagnini@gmail.com>
2026-07-30 22:55:35 +0200

    Refactor lolsnes_screen.h header comments
    Updated header comments and removed unused palette length definition.

--- arm9/source/lolsnes_screen.h
+++ arm9/source/lolsnes_screen.h
@@ -4,11 +4,10 @@
 //======================================================================
 //
 //	lolsnes_screen, 256x192@8, 
-//	+ palette 256 entries, not compressed
 //	+ 768 tiles not compressed
-//	Total size: 512 + 49152 = 49664
+//	Total size: 49152 = 49152
 //
-//	Time-stamp: 2026-07-30, 22:50:28
+//	Time-stamp: 2026-07-30, 22:54:03
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -20,4 +19,6 @@
 #define lolsnes_screenTilesLen 49152
 extern const unsigned short lolsnes_screenTiles[24576];
 
-#define lolsnes_screenPalLen 51
+#endif // GRIT_LOLSNES_SCREEN_H
+
+//}}BLOCK(lolsnes_screen)
