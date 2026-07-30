710f4d4b80c5613988765f8416285dd9defc1c32
git-effl <serenatamagnini@gmail.com>
2026-07-30 23:00:00 +0200

    Update lolsnes_screen.h with new tile and map sizes

--- arm9/source/lolsnes_screen.h
+++ arm9/source/lolsnes_screen.h
@@ -4,10 +4,12 @@
 //======================================================================
 //
 //	lolsnes_screen, 256x192@8, 
-//	+ 768 tiles not compressed
-//	Total size: 49152 = 49152
+//	+ palette 256 entries, not compressed
+//	+ 1 tiles (t|f|p reduced) not compressed
+//	+ regular map (flat), not compressed, 32x24 
+//	Total size: 512 + 64 + 1536 = 2112
 //
-//	Time-stamp: 2026-07-30, 22:54:03
+//	Time-stamp: 2026-07-30, 22:57:28
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -16,8 +18,14 @@
 #ifndef GRIT_LOLSNES_SCREEN_H
 #define GRIT_LOLSNES_SCREEN_H
 
-#define lolsnes_screenTilesLen 49152
-extern const unsigned short lolsnes_screenTiles[24576];
+#define lolsnes_screenTilesLen 64
+extern const unsigned short lolsnes_screenTiles[32];
+
+#define lolsnes_screenMapLen 1536
+extern const unsigned short lolsnes_screenMap[768];
+
+#define lolsnes_screenPalLen 512
+extern const unsigned short lolsnes_screenPal[256];
 
 #endif // GRIT_LOLSNES_SCREEN_H
 
